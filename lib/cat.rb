2e52e970ecc666a7746e9fa64ecf11bff36dcdd9
Stef Spears <spears.sj87@gmail.com>
2019-10-13 21:31:46 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,11 +2,16 @@ class Cat
   attr_accessor :owner, :mood
   attr_reader :name
   
+  @@all = []
+  
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
   end 
   
+  def self.all 
+    @@all 
+  end 
   
 end
