e2cf76003d556e55c25c8459edc75e10df079811
Stef Spears <spears.sj87@gmail.com>
2019-10-13 21:22:44 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,8 @@
 class Owner
   attr_reader :name, :species
   
+  @@all = []
+  
   def initialize(name)
     @name = name
     @species = "human"
@@ -8,5 +10,9 @@ class Owner
   
   def say_species
     p "I am a #{@species}."
+  end 
+  
+  def self.all 
+    @@all 
   end 
 end
